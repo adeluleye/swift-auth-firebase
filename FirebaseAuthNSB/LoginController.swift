0e2a443274e1fd6544c85d6d8b023ca637c0d871
adeluleye <adelu4real@gmail.com>
2018-10-04 16:38:27 +0100

    hide navigation bar in login screen on viewWillAppear

--- FirebaseAuthNSB/LoginController.swift
+++ FirebaseAuthNSB/LoginController.swift
@@ -19,6 +19,11 @@ class LoginController: UIViewController {
         setupView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
     func setupView() {
         let mainView = LoginView(frame: self.view.frame)
         self.loginView = mainView
