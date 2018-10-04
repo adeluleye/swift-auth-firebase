0ce268b0ad22afebd0904a9e73236529a26a8202
adeluleye <adelu4real@gmail.com>
2018-10-04 23:25:32 +0100

    create a signup view class

---
+++ FirebaseAuthNSB/SignUp/SignUpView.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpView.swift
+//  FirebaseAuthNSB
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/4/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        
+    }
+    
+}
