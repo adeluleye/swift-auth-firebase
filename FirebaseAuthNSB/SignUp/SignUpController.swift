76552ddc7d9ceac77d086b10e25d9ef9b36748e5
adeluleye <adelu4real@gmail.com>
2018-10-04 23:23:06 +0100

    create a signup controller with viewDidLoad

---
+++ FirebaseAuthNSB/SignUp/SignUpController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpController.swift
+//  FirebaseAuthNSB
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/4/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
