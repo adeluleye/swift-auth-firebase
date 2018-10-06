86fdf143bcdc92f31f3b6ee9cb9b54b92db10180
adeluleye <adelu4real@gmail.com>
2018-10-06 18:49:23 +0100

    refactor signup presses to use the singleton firebaseAPI

--- FirebaseAuthNSB/SignUp/SignUpController.swift
+++ FirebaseAuthNSB/SignUp/SignUpController.swift
@@ -15,14 +15,8 @@ class SignUpController: UIViewController {
     
     var darkMode = false
     
-    var defaults = UserDefaults.standard
-    
-    // get ref to user name in Database
-    var ref: DatabaseReference!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref = Database.database().reference()
         
         setupView()
     }
@@ -47,24 +41,13 @@ class SignUpController: UIViewController {
         guard let password = signUpView.passwordTextField.text else { return }
         guard let name = signUpView.nameTextField.text else { return }
         
-        let userData: [String: Any] = [
-            "name": name
-        ]
-        
         // implement user creation
-        Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
-            
-            if let err = err {
-                print(err.localizedDescription)
+        FirebaseAPI.shared.signUp(name: name, email: email, password: password) { [weak self] (err) in
+            if err != nil {
+                // error alert
+                print(err?.localizedDescription)
             } else {
-                // uid is optional, therefore unwrap
-                guard let uid = result?.user.uid else { return }
-                self.ref.child("users/\(uid)").setValue(userData)
-                self.defaults.set(false, forKey: "UserIsLoggedIn")
-                print("User account created successfully", uid)
-                
-                // dismiss the control after account signup
-                self.dismiss(animated: true, completion: nil)
+                self?.dismiss(animated: true, completion: nil)
             }
         }
         
